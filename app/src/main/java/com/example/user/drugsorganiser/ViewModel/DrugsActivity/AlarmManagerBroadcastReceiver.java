cadfd97039f86939ee52aef2039d89a1ec50d6cc
Patryk Wrobel <Patryk.wr@wp.pl>
2017-05-16 22:25:48 +0200

    alarm implementation

--- app/src/main/java/com/example/user/drugsorganiser/ViewModel/DrugsActivity/AlarmManagerBroadcastReceiver.java
+++ app/src/main/java/com/example/user/drugsorganiser/ViewModel/DrugsActivity/AlarmManagerBroadcastReceiver.java
@@ -1,13 +1,19 @@
 package com.example.user.drugsorganiser.ViewModel.DrugsActivity;
 
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.support.v7.app.NotificationCompat;
 import android.widget.Toast;
+
+import com.example.user.drugsorganiser.R;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
@@ -39,25 +45,42 @@ public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
         msgStr.append(formatter.format(new Date()));
 
         Toast.makeText(context, msgStr, Toast.LENGTH_LONG).show();
+        CreateNotification(context, "Lek1", "2 łyżeczki, po posiłku", "Wiadomość budzika", DrugsActivity.class, 1);
 
         //Release the lock
         wl.release();
 
     }
-    public void SetAlarm(Context context)
-    {
+
+    public  void CreateNotification (Context context, String drugName, String drugDescription, String msgAlert, Class <?> cls, int id) {
+        Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
+        PendingIntent notifIntent = PendingIntent.getActivity(context,0, new Intent(context,cls),0);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+        mBuilder.setSmallIcon(R.drawable.ic_alarm_on_black_24dp);
+        mBuilder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_alarm_on_black_24dp));
+        mBuilder.setContentTitle(drugName);
+        mBuilder.setTicker(msgAlert);
+        mBuilder.setContentText(drugDescription);
+        mBuilder.setContentIntent(notifIntent);
+        mBuilder.setDefaults(NotificationCompat.DEFAULT_SOUND);
+//        mBuilder.setAutoCancel(true);
+        mBuilder.setVibrate(new long[]{1000, 1000});
+        mBuilder.setWhen(System.currentTimeMillis());
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(id, mBuilder.build());
+    }
+
+    public void SetAlarm(Context context) {
         AlarmManager alarmManager=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
         intent.putExtra(ONE_TIME, Boolean.FALSE);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
-        //After 30 seconds
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 5 , pi);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 5 , pi); //AlarmManager.INTERVAL_DAY
     }
 
-    public void CancelAlarm(Context context)
-    {
+    public void CancelAlarm(Context context) {
         Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
-        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.cancel(sender);
     }
@@ -66,7 +89,7 @@ public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
         AlarmManager alarmManager =(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
         intent.putExtra(ONE_TIME, Boolean.TRUE);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+2000, pi);
     }
 }
