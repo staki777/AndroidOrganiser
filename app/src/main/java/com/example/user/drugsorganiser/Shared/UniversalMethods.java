148121c16ea2b08efff37c8be91cf9e4699d9396
golebiewskak <golebiewskak@student.mini.pw.edu.pl>
2017-05-30 14:03:17 +0200

    DateTime toString repair

--- app/src/main/java/com/example/user/drugsorganiser/Shared/UniversalMethods.java
+++ app/src/main/java/com/example/user/drugsorganiser/Shared/UniversalMethods.java
@@ -8,6 +8,6 @@ import org.joda.time.DateTime;
 
 public class UniversalMethods {
     public static String DateTimeToString(DateTime date){
-        return date.getDayOfMonth()+"."+(date.getMonthOfYear()<10?"0":"")+date.getMonthOfYear()+"."+date.getYear()+" "+date.getHourOfDay()+":"+((date.getMinuteOfHour()<10)?"0":"")+date.getMinuteOfHour();
+        return (date.getDayOfMonth()<10?"0":"")+date.getDayOfMonth()+"."+(date.getMonthOfYear()<10?"0":"")+date.getMonthOfYear()+"."+date.getYear()+" "+date.getHourOfDay()+":"+((date.getMinuteOfHour()<10)?"0":"")+date.getMinuteOfHour();
     }
 }
